fcdebab18093528a5b8a723046bb8311ff207550
ducvv <ducvv@mcrew-tech.com>
2021-09-03 22:05:16 +0700

    Migration 1 Table

--- database.md
+++ database.md
@@ -1,2 +1,11 @@
+1/
 CREATE DATABASE `migration_test`
 USE `migration_test`;
+
+php artisan make:migration customer
+php artisan make:factory CustomerFactory
+php artisan make:seeder CustomerSeeder
+php artisan make:model CustomerModel
+
+php artisan migrate;
+php artisan db:seed
---
+++ database/migrations/2021_09_03_122703_customer.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Customer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('description');
+            $table->rememberToken();
+            $table->string('password');
+
+            $table->timestamp('time_1')->nullable();
+            $table->timestamp('time_2')->nullable();
+            $table->timestamp('time_3')->nullable();
+            $table->timestamp('time_4')->nullable();
+            
+            $table->timestamps();
+            $table->integer('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
